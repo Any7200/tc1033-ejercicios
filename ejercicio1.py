85dd28ea93feb105de7adec5c0f03d786ae6219e
Ana Laura Rodríguez <anarj14@hotmail.com>
2019-12-08 23:03:40 -0600

    Implementación de la función run

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,6 +1,17 @@
+#A01366974
 def greet(_name):
     print("Hello",_name)
+def run(_number):
+    if _number >= 0 and _number <= 10:
+        value = 0
+        while value!=_number:
+            print("Running")
+            value+=1
 print("Nombre?")
 name = str(input())
 obj1 = greet(name)
 obj1
+print("Número?")
+number = int(input())
+obj2 = run(number)
+obj2
