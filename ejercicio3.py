3194f708d3023018a3b75a49260f82922b9446bd
Ana Laura Rodríguez <anarj14@hotmail.com>
2019-12-08 23:41:34 -0600

    Implementado clase Vehiculos

---
+++ ejercicio3.py
@@ -0,0 +1,6 @@
+class Vehiculos:
+    def __init__(self, _matricula, _medio, _capacidad, _velocidad_max):
+        self.matricula = _matricula
+        self.medio = _medio
+        self.capacidad = _capacidad
+        self.velocidad_max = _velocidad_max
